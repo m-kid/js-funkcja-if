61e9d77be685480f4370089e567d7318f8c31869
m-kid <maciej.kidawa2@gmail.com>
2016-10-09 15:28:46 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a = prompt('Podaj dowolną liczbę'),
+    h = a/3,
+    triangleArea = a*h/2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+console.log(triangleArea)
